6a679470316de444f9655b105f52e783e015dc9e
orest-forest <danilchenko.orest@gmail.com>
2020-03-17 16:43:56 +0200

    First commit.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 import App from "./components/App.js";
+import configureStore from './store.jsx';
+import {BrowserRouter} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore();
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
+
+
+render(app, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.join(__dirname, "/dist"),
-        filename: "index-bundle.js"
+        path: path.resolve(__dirname, "/dist"),
+        filename: "index-bundle.js",
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -20,6 +21,9 @@ module.exports = {
             }
         ]
     },
+    devServer: {
+    historyApiFallback: true,
+},
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html"
